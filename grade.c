bccaf6cdc78074af24470625ad16361a243068e2
Rhitxm <shyamasree31@gmail.com>
2026-09-22 19:24:56 +0530

    Update grade.c

--- grade.c
+++ grade.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int marks;
+   printf("enter your marks:");
+    scanf("%d", &marks);
+    if (marks>30){
+        printf("PASS");
+    }
+    else if (marks<=30){
+        printf("FAIL");
+    }
+    return 0;
+}
