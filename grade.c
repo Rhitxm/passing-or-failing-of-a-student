3c3f7714d4419f7f8ab94ea3e4694a8b29d38140
Rhitxm <shyamasree31@gmail.com>
2026-09-22 20:15:05 +0530

    Update grade.c

--- grade.c
+++ grade.c
@@ -12,3 +12,26 @@ int main() {
     }
     return 0;
 }
+//another similar program
+int main() {
+    int marks;
+   printf("enter your marks:");
+    scanf("%d", &marks);
+    if (marks<30){
+        printf("C Grade\n");
+    }
+    else if (30<=marks && marks<70){
+        printf("B Grade\n");
+    }
+    else if (70<=marks && marks<90){
+        printf("A Grade\n");
+    }
+    else if (90<=marks && marks<=100){
+        printf("A+ Grade\n");
+    }
+    else{
+        printf("invalid\n");
+    }
+        
+    return 0;
+}
